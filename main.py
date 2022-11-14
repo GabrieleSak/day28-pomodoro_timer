57dd17096845476651cba33d452ce1f3b7d91940
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-11-14 11:23:11 +0200

    Add canvas with tomato image, timer and check mark labels and Start, Reset buttons

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,3 +16,45 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+def start_clicked():
+    pass
+
+def reset_clicked():
+    pass
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", command=start_clicked)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", command=reset_clicked)
+reset_button.grid(column=2, row=2)
+
+check_mark = "✔"
+check_mark_label = Label(text=check_mark, fg=GREEN, bg=YELLOW)
+check_mark_label.grid(column=1, row=3)
+
+
+
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
